c67d4fbf42cc9ea29c5e0debd90a280fbc856276
xTeKc <bcd3v@pm.me>
2021-07-09 20:19:00 -0400

    add eth scan

---
+++ ethSCscan.py
@@ -0,0 +1,12 @@
+import requests
+import selenium
+
+
+eca = "ethereum_contract_address"
+
+
+def eth_contract_address(eca):
+    input("Enter Ethereum Contract Address: ")
+
+
+eth_contract_address(eca)
