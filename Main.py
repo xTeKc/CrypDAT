ec3724b9eeec395849010263eefcb8d09588d103
xTeKc <bcd3v@pm.me>
2021-07-09 20:59:03 -0400

    shorten import name

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import Blockchains.Binance as bnb
 import Blockchains.Cardano as ada
 import Blockchains.Ethereum as eth
-import Blockchains.Fantom
+import Blockchains.Fantom as ftm
 import Blockchains.HarmonyShard
 import Blockchains.Huobi
 import Blockchains.Polygon
