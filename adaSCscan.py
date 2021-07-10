54ed3cefb0eac8f8e5517e39e12f0278cfe807be
xTeKc <bcd3v@pm.me>
2021-07-09 20:25:55 -0400

    add ada scan

---
+++ adaSCscan.py
@@ -0,0 +1,12 @@
+import requests
+import selenium
+
+
+acs = "cardano_contract_address"
+
+
+def cardano_contract_address(acs):
+    input("Enter Cardano Contract Address: ")
+
+
+cardano_contract_address(acs)
