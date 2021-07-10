3a7503d3b5c10c49def465c7732a7de349833f98
xTeKc <bcd3v@pm.me>
2021-07-09 21:38:04 -0400

    refactor

--- Blockchains/Huobi.py
+++ Blockchains/Huobi.py
@@ -2,11 +2,11 @@ import requests
 import selenium
 
 
-hu_ca = "huobi_contract_address"
+huo_ca = "huobi_contract_address"
 
 
-def huobi_contract_address(hu_ca):
+def huobi_contract_address(huo_ca):
     input("Enter Huobi Contract Address: ")
 
 
-huobi_contract_address(hu_ca)
+huobi_contract_address(huo_ca)
