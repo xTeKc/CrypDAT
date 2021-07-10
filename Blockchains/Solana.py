2e003c94510db8244c64a9317a2a78a84882a018
xTeKc <bcd3v@pm.me>
2021-07-09 21:34:26 -0400

    add solana scan

---
+++ Blockchains/Solana.py
@@ -0,0 +1,12 @@
+import requests
+import selenium
+
+
+sol_ca = "solana_contract_address"
+
+
+def solana_contract_address(sol_ca):
+    input("Enter Solana Contract Address: ")
+
+
+solana_contract_address(sol_ca)
