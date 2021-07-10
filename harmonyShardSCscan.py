ebec0996cfe6494c51042dc2e74461e0d72d2b06
xTeKc <bcd3v@pm.me>
2021-07-09 20:21:32 -0400

    add harmony shard scan

---
+++ harmonyShardSCscan.py
@@ -0,0 +1,12 @@
+import requests
+import selenium
+
+
+hsca = "harmonyShard_contract_address"
+
+
+def harmonyShard_contract_address(hsca):
+    input("Enter Harmony Shard Contract Address: ")
+
+
+harmonyShard_contract_address(hsca)
