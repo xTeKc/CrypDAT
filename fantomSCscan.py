bb5eb9ab124751628fe775f9f4304d6385ae6180
xTeKc <bcd3v@pm.me>
2021-07-09 20:20:26 -0400

    add fantom scan

---
+++ fantomSCscan.py
@@ -0,0 +1,12 @@
+import requests
+import selenium
+
+
+fca = "fantom_contract_address"
+
+
+def fantom_contract_address(fca):
+    input("Enter Fantom Contract Address: ")
+
+
+fantom_contract_address(fca)
