e18ba8fe3fd671c6ee2c1062fe5be14e03ee5610
xTeKc <bcd3v@pm.me>
2021-07-09 20:22:55 -0400

    add polygon scan

---
+++ polygonSCscan.py
@@ -0,0 +1,12 @@
+import requests
+import selenium
+
+
+pca = "polygon_contract_address"
+
+
+def polygon_contract_address(pca):
+    input("Enter Polygon Contract Address: ")
+
+
+polygon_contract_address(pca)
