d23664afac9731986e090f2a8249209c02414204
xTeKc <bcd3v@pm.me>
2021-07-09 21:31:00 -0400

    refactor

--- Blockchains/Binance.py
+++ Blockchains/Binance.py
@@ -2,11 +2,11 @@ import requests
 import selenium
 
 
-bnb_ca = "binance_contract_address"
+bsc_ca = "binance_contract_address"
 
 
-def binance_contract_address(bnb_ca):
+def binance_contract_address(bsc_ca):
     input("Enter Binance Contract Address: ")
 
 
-binance_contract_address(bnb_ca)
+binance_contract_address(bsc_ca)
