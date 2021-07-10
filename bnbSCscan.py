2c6e32f64a133c2e0daf43416f0e24dffceed13d
xTeKc <bcd3v@pm.me>
2021-07-09 20:19:45 -0400

    add bnb scan

---
+++ bnbSCscan.py
@@ -0,0 +1,12 @@
+import requests
+import selenium
+
+
+bca = "binance_contract_address"
+
+
+def bnb_contract_address(bca):
+    input("Enter Binance Contract Address: ")
+
+
+bnb_contract_address(bca)
