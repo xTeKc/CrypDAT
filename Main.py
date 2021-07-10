768188a252b50d4d4f066cfed156ebe52f17e968
xTeKc <bcd3v@pm.me>
2021-07-09 21:35:22 -0400

    add solana import

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ import Blockchains.Fantom as ftm
 import Blockchains.HarmonyShard as hs
 import Blockchains.Huobi as hu
 import Blockchains.Polygon as poly
+import Blockchains.Solana as sol
 import Blockchains.xDai as xdai
 
 
