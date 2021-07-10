418be237dcb363b7e990346f70aa410468d63189
xTeKc <bcd3v@pm.me>
2021-07-09 21:38:30 -0400

    refactor

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import Blockchains.Cardano as ada
 import Blockchains.Ethereum as eth
 import Blockchains.Fantom as ftm
 import Blockchains.HarmonyShard as hs0
-import Blockchains.Huobi as hu
+import Blockchains.Huobi as huo
 import Blockchains.Polygon as poly
 import Blockchains.Solana as sol
 import Blockchains.xDai as xdai
