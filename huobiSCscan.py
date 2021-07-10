1ae0932b6dc6a8040becd9d6e70b9e2add27b636
xTeKc <bcd3v@pm.me>
2021-07-09 20:22:13 -0400

    add huobi scan

---
+++ huobiSCscan.py
@@ -0,0 +1,12 @@
+import requests
+import selenium
+
+
+hca = "huobi_contract_address"
+
+
+def huobi_contract_address(hca):
+    input("Enter Huobi Contract Address: ")
+
+
+huobi_contract_address(hca)
