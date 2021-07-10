63526e3e132a5bce0ecdcc7e2df3ff486d66b0a3
xTeKc <bcd3v@pm.me>
2021-07-09 21:00:12 -0400

    shorten import name

--- Main.py
+++ Main.py
@@ -5,5 +5,5 @@ import Blockchains.Fantom as ftm
 import Blockchains.HarmonyShard as hs
 import Blockchains.Huobi as hu
 import Blockchains.Polygon as poly
-import Blockchains.xDai
+import Blockchains.xDai as xdai
 
