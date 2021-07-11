18c0f329d3d6b20a8fcd11320d36f2caea634d5f
xTeKc <bcd3v@pm.me>
2021-07-11 17:28:38 -0400

    enable ability to search by contract address entered

--- Blockchains/Polygon.py
+++ Blockchains/Polygon.py
@@ -7,9 +7,13 @@ from decouple import config
 
 poly_ca = config('POLYGON_API_KEY')
 
+poly_search = input('Enter Polygon Contract Address: ')
 
-def polygon_contract_address(poly_ca):
-    input("Enter Polygon Contract Address: ")
+response = requests.get(f'https://api.polygonscan.com/api?module=contract&action=getsourcecode&address={poly_search}&apikey={poly_ca}')
 
 
-polygon_contract_address(poly_ca)
+def polygon_contract_address(poly_ca, response):
+    print(response.json())
+
+
+polygon_contract_address(poly_ca, response)
