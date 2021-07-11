c67baff801d572fb36e61f28721835f8b9174e35
xTeKc <bcd3v@pm.me>
2021-07-11 16:51:24 -0400

    enable ability to search by contract address entered

--- Blockchains/Binance.py
+++ Blockchains/Binance.py
@@ -7,9 +7,14 @@ from decouple import config
 
 bsc_ca = config('BINANCE_API_KEY')
 
+bsc_search = input('Enter Binance Contract Address: ')
 
-def binance_contract_address(bsc_ca):
-    input("Enter Binance Contract Address: ")
+response = requests.get(f'https://api.bscscan.com/api?module=contract&action=getsourcecode&address={bsc_search}&apikey={bsc_ca}')
+
+def binance_contract_address(bsc_ca, response):
+    print(response.json())
 
 
-binance_contract_address(bsc_ca)
+binance_contract_address(bsc_ca, response)
+
+
