73786f2ac7aca14c756340973b507a829bc4dcc0
xTeKc <bcd3v@pm.me>
2021-07-11 17:08:44 -0400

    enable ability to search by contract address entered

--- Blockchains/Ethereum.py
+++ Blockchains/Ethereum.py
@@ -8,8 +8,12 @@ from decouple import config
 eth_ca = config('ETHEREUM_API_KEY')
 
 
-def ethereum_contract_address(eth_ca):
-    input("Enter Ethereum Contract Address: ")
+eth_search = input('Enter Ethereum Contract Address: ')
+
+response = requests.get(f'https://api.etherscan.io/api?module=contract&action=getsourcecode&address={eth_search}&apikey={eth_ca}')
+
+def ethereum_contract_address(eth_ca, response):
+    print(response.json())
 
 
-ethereum_contract_address(eth_ca)
+ethereum_contract_address(eth_ca, response)
